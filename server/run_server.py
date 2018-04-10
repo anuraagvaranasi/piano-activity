a3ccf8656db4ee507069c3c4f87a5c152b2ae8df
anuraagvaranasi <anuraag.varanasi@gmail.com>
2018-04-10 23:27:49 +1000

    added a class for notes to easily know data for student
    will add a constructor later to take in lists, so that students can
    provide their own list of notes

--- server/run_server.py
+++ server/run_server.py
@@ -3,6 +3,31 @@ from flask import Flask, request, render_template, send_from_directory, jsonify
 app = Flask(__name__, static_folder=None)
 
 CLIENT_FOLDER = os.path.abspath('../client/build')
+default_sequence = ['Bb','A','D','E','F#','G','Ab']
+
+class Notes:
+
+    def __init__(self):
+        self.position = 0
+        self.notes = default_sequence
+        self.results = []
+
+    def end(self):
+        return self.position == len(self.notes)
+    
+    def next_note(self):
+        return_note = self.notes[self.position]
+        self.position += 1
+        return return_note
+
+    def record_result(self,note_played):
+        #position is 1 further than result table
+        #as we move the position in the next_note method
+        print(self.position)
+        #result_position = self.position-1
+        #self.results[result_position] = (self.notes[result_position] in note_played) 
+
+test = Notes()
 
 @app.route('/')
 def welcome():
@@ -14,13 +39,14 @@ def note():
 
     if request.method == 'POST':
         notes = request.get_json()
-        if 'C#' in notes:
-            result = True
-        else:
-            result = False
-    else:
-        result = {'note': 'C#'}
+
+        test.record_result(notes)
+
     
+    result = {'note': test.next_note()}
+    print(test.results)
+    print(result)
+
     return jsonify(result)
 
 @app.route('/piano/', methods=['GET'])
