5cdbf076b8c63f3bf0eefc5f89dba58380494536
anuraagvaranasi <anuraag.varanasi@gmail.com>
2018-04-10 23:52:46 +1000

    disabled serviceWorker (enable it later, annoying for testing)

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 const NOTE_URL = 'http://localhost:5000/note';
 
@@ -26,4 +26,4 @@ ReactDOM.render(
   />,
   document.getElementById('root')
 );
-registerServiceWorker();
+unregister();
