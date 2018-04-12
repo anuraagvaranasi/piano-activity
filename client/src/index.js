efdd9da6873543946df97afda0d1abd9b8cf3df8
anuraagvaranasi <anuraag.varanasi@gmail.com>
2018-04-12 22:28:11 +1000

    forgot to add index.js for cookie sending

--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,13 @@ import { unregister } from './registerServiceWorker';
 const NOTE_URL = 'http://localhost:5000/note';
 
 const fetchNextNote = () => 
-  fetch(NOTE_URL).then(response => response.json());
+  fetch(NOTE_URL,{
+    credentials: 'same-origin',
+  }).then(response => response.json());
 
 const checkAnswer = (answer) => 
   fetch(NOTE_URL, {
+    credentials: 'same-origin',
     body: JSON.stringify(answer),
     cache: 'no-cache',
     headers: {
