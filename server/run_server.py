fef411850be6f84e07a44db787c796a155d0fa22
anuraagvaranasi <anuraag.varanasi@gmail.com>
2018-04-12 21:26:23 +1000

    added a login function with a SQL database

--- server/run_server.py
+++ server/run_server.py
@@ -1,14 +1,35 @@
 import os
-from flask import Flask, request, render_template, send_from_directory, jsonify, make_response
-from uuid import uuid4
-app = Flask(__name__, static_folder=None)
+from flask import Flask, request, render_template, send_from_directory,\
+ jsonify, make_response,url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+import hashlib
 
+
+
+
+app = Flask(__name__, static_folder=None)
 CLIENT_FOLDER = os.path.abspath('../client/build')
+
+#database stuff
+dbPath = os.path.abspath('piano.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbPath
+#disable event system, reduces overhead 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True,nullable=False)
+    password = db.Column(db.String(256),unique=False,nullable=False)
+
+    def __repr__(self):
+        return self.username
+
 default_sequence = ['C#','A','D','E','F#','G','Ab']
 #default_sequence = ['C#']
 
 class Notes:
-
     def __init__(self):
         self.position = 0
         self.notes = default_sequence
@@ -31,34 +52,85 @@ class Notes:
         else:
             self.results.append(False)    
             return False
+
     def final_stats(self):
         return {'note':'Done!','correct':len(self.notes),'total':len(self.results)}
 
-#where the data for notes will be stored  
-test = Notes()
+    def restart(self):
+        self.position = 0
+        self.results.clear()
+
+    def last_element(self):
+        return self.results[-1]
+
+#where the state of the program will be stored
+#for when users log in then log out
 userDictionary = {}
 
-@app.route('/')
-def welcome():
-    sessionID = str
-    resp = make_response(render_template('welcome.html'))
-    resp.set_cookie('sessionID',sessionID)
-    resp.set_cookie('username','anuraag')
-    userDictionary[sessionID] = Notes()
-    return resp
+#login to piano app, also stores state if logged in before and wasnt completed
+@app.route('/', methods=['GET','POST'])
+def login():
+    if request.method == 'GET': #GET = display login page
+        return render_template('welcome.html')
 
+    else: #POST, read login information
+        print(request.form)
+        user = request.form['username']
+        pwd = request.form['password']
 
+        if user_exists(user) and hash(pwd) == get_pass(user):
+            resp = make_response(send_from_directory(CLIENT_FOLDER, 'index.html'))
+            resp.set_cookie('username',user)
+            #create a new class only if it doesnt exist already
+            if user not in userDictionary:
+                userDictionary[user] = Notes()
+            return resp
 
+        else:
+            return render_template('welcome.html',incorrect=True)
+
+#register a new user
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else: #read register information and store in database
+        if register_user(request.form['username'],request.form['password']):
+            #succesfully registered, redirect to login page
+            return redirect(url_for('login'))
+        else:
+            return render_template('register.html',incorrect=True)
+
+#get sha256 hash of a string to store/compare password
+#since we should never store password in plaintext
+def hash(string):
+    return hashlib.sha256(string.encode('utf-8')).hexdigest()
+
+#check if a user exists in database
+def user_exists(user):
+    if User.query.filter_by(username=user).first() is None:
+        return False
+    else:
+        return True
+
+#get password from database (which is sha256(pass))
+def get_pass(user):
+    return db.session.query(User.password).filter(User.username == user).scalar()
+
+#register user in database, or return false if username already exists
+def register_user(user,pwd):
+    if user_exists(user) == False:
+        new_user = User(username=user,password=hash(pwd))
+        db.session.add(new_user)
+        db.session.commit()
+        return True
+    else:
+        print("cannot register user " + user)
+        return False
 
 @app.route('/note', methods=['GET', 'POST'])
 def note():
-
-    id = request.cookies.get('sessionID')
-    print(request.cookies.get('username'))
-    print(id)
-    print(request.cookies)
-    print("VVCONFUSED???")
-    currentUser = test
+    currentUser = userDictionary[request.cookies.get('username')]
     result = None
 
     #if req method = post, it wants to check if note clicked is correct
@@ -71,7 +143,9 @@ def note():
         #but first check if there are still notes to get
         #return end string if complete
         if (currentUser.end()):
-            return jsonify(currentUser.final_stats())
+            results = currentUser.final_stats()
+            currentUser.restart()
+            return jsonify(results)
 
         #else send next note
         result = {'note': currentUser.next_note()}
@@ -79,9 +153,6 @@ def note():
     return jsonify(result)
 
 
-@app.route('/piano/', methods=['GET'])
-def serve_app():
-    return send_from_directory(CLIENT_FOLDER, 'index.html')
 
 @app.route('/<path:path>', methods=['GET'])
 def serve_static(path):
